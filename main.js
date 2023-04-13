c1999fa860ea53ca01c80f50d7a94d7f040a3978
Nguyễn Nghiệp <nghiep2k3>
2023-04-13 23:05:58 +0700

    code

--- main.js
+++ main.js
@@ -39,21 +39,7 @@ ulList2.addEventListener("click", function() {
 
 // });
 
-var decreaseButton = document.querySelector(".decrease");
-var increaseButton = document.querySelector(".increase");
-var quantityInput = document.querySelector(".quantity");
 
-decreaseButton.addEventListener("click", function() {
-  var currentValue = parseInt(quantityInput.value);
-  if (currentValue > 1) {
-    quantityInput.value = currentValue - 1;
-  }
-});
-
-increaseButton.addEventListener("click", function() {
-  var currentValue = parseInt(quantityInput.value);
-  quantityInput.value = currentValue + 1;
-})
 
 var a = document.querySelector('form');
 console.log(a);
